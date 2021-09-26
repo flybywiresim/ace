c40b782bbfe22c0d330a6983fbeb7345be535683
ErickSharp <titaninmc@gmail.com>
2021-09-26 10:31:58 -0500

    get rid of onDelete

--- src/renderer/Pages/ProjectHome/ProjectWorkspace.tsx
+++ src/renderer/Pages/ProjectHome/ProjectWorkspace.tsx
@@ -240,7 +240,6 @@ export const ProjectWorkspace = () => {
                                                     key={canvasElement.title}
                                                     instrumentFrame={canvasElement}
                                                     zoom={zoom}
-                                                    onDelete={() => handleDeleteCanvasElement(canvasElement)}
                                                     onUpdate={handleUpdateCanvasElement}
                                                     selected={selectedCanvasElements.includes(canvasElement)}
                                                 />

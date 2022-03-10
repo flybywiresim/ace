12048ab895eb47d58cf5d9d3f7386f4b67b9ed5b
Tacotakedown <87836366+Tacotakedown@users.noreply.github.com>
2022-03-09 17:27:35 -0700

    reformatted simvar window

--- src/renderer/Pages/ProjectHome/Components/SimVars/Controls/SimVarControlsMenu.tsx
+++ src/renderer/Pages/ProjectHome/Components/SimVars/Controls/SimVarControlsMenu.tsx
@@ -1,43 +1,46 @@
-import React, { FC } from 'react';
-import { IconPlus } from '@tabler/icons';
-import { SideMenu } from '../../Framework/Toolbars';
-import { SimVarControlElement } from './SimVarControlElement';
-import { useProjectDispatch, useProjectSelector } from '../../../Store';
-import { SimVarControl } from '../../../../../../shared/types/project/SimVarControl';
-import { deleteControl } from '../../../Store/actions/simVarElements.actions';
+import React, { FC } from "react";
+import { IconPlus } from "@tabler/icons";
+import { SideMenu } from "../../Framework/Toolbars";
+import { SimVarControlElement } from "./SimVarControlElement";
+import { useProjectDispatch, useProjectSelector } from "../../../Store";
+import { SimVarControl } from "../../../../../../shared/types/project/SimVarControl";
+import { deleteControl } from "../../../Store/actions/simVarElements.actions";
 
 export interface SimVarControlsMenuProps {
-    onOpenNewControlMenu?: () => void,
-    onStartEditingControl?: (control: SimVarControl) => void,
+  onOpenNewControlMenu?: () => void;
+  onStartEditingControl?: (control: SimVarControl) => void;
 }
 
-export const SimVarControlsMenu: FC<SimVarControlsMenuProps> = ({ onOpenNewControlMenu, onStartEditingControl }) => {
-    const simVarControls = useProjectSelector((state) => state.simVarElements);
-    const projectDispatch = useProjectDispatch();
+export const SimVarControlsMenu: FC<SimVarControlsMenuProps> = ({
+  onOpenNewControlMenu,
+  onStartEditingControl,
+}) => {
+  const simVarControls = useProjectSelector((state) => state.simVarElements);
+  const projectDispatch = useProjectDispatch();
 
-    return (
-        <SideMenu className="w-[420px] bg-navy z-50">
-            <h2 className="mb-3 font-medium">SimVars</h2>
+  return (
+    <SideMenu className="w-[420px] h-[100%] bg-navy z-50 overflow-auto">
+      <h2 className="mb-3 font-medium">SimVars</h2>
 
-            <div className="flex flex-col divide-y divide-gray-700">
-                {simVarControls.map((control) => (
-                    <SimVarControlElement
-                        key={control.__uuid}
-                        simVarControl={control}
-                        onEdit={() => onStartEditingControl(control)}
-                        onDelete={() => projectDispatch(deleteControl(control))}
-                    />
-                ))}
-            </div>
+      <div className="flex flex-col divide-y divide-gray-700">
+        {simVarControls.map((control) => (
+          <SimVarControlElement
+            key={control.__uuid}
+            simVarControl={control}
+            onEdit={() => onStartEditingControl(control)}
+            onDelete={() => projectDispatch(deleteControl(control))}
+          />
+        ))}
+      </div>
 
-            <div
-                className="flex flex-row justify-center items-center gap-x-2 py-3 mt-2 border-2 border-dashed text-gray-500 border-gray-500 rounded-md"
-                onClick={onOpenNewControlMenu}
-            >
-                <IconPlus size={30} />
+      <div
+        className="flex flex-row justify-center items-center gap-x-2 py-3 mt-2 border-2 border-dashed text-gray-500 border-gray-500 rounded-md"
+        onClick={onOpenNewControlMenu}
+      >
+        <IconPlus size={30} />
 
-                <span className="text-xl">Add SimVar Control</span>
-            </div>
-        </SideMenu>
-    );
+        <span className="text-xl">Add SimVar Control</span>
+      </div>
+    </SideMenu>
+  );
 };

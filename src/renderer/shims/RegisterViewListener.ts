3118f8e8b9bc447ad0fe93d425030c7c06528597
Benjamin Dupont <benjozorkfr@gmail.com>
2022-04-24 11:00:29 -0400

    add some more ViewListener stubs to avoid crashes

--- src/renderer/shims/RegisterViewListener.ts
+++ src/renderer/shims/RegisterViewListener.ts
@@ -10,9 +10,29 @@ export class ViewListener {
         this.coherent = coherent;
     }
 
+    public trigger(_name: string, ..._data: any[]) {
+        // noop
+    }
+
     public triggerToAllSubscribers(name: string, ...data: any[]) {
         if (this.name === 'JS_LISTENER_SIMVARS') {
             this.coherent.trigger(name, ...data);
         }
+    }
+
+    public on(_event: string, _callback: (...data: any[]) => void, _context: any): void {
+        // noop
+    }
+
+    public off(_event: string, _callback: (...data: any[]) => void, _context: any): void {
+        // noop
+    }
+
+    public call(_name: string, _args: any[]): void {
+        // noop
+    }
+
+    public unregister(): void {
+        // noop
     }
 }
